5515aa1a14b0407dacc5cacfc0dd72422bc00536
Julian <79467458+ZehJulianLp@users.noreply.github.com>
2023-05-30 19:22:02 +0200

    Added Skins for Shop

--- shop/mc-skin/script.js
+++ shop/mc-skin/script.js
@@ -1,47 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetchSkinOptions();
     showImage();
 });
 
-function fetchSkinOptions() {
-    fetch("https://api.mineskin.eu/get/list/Dwarslooper")
-        .then(response => response.json())
-        .then(data => {
-            // data enthält alle Skin-Optionen für Dwarslooper
-            processSkinOptions(data);
-        })
-        .catch(error => {
-            console.error("Fehler beim Abrufen der Skin-Optionen:", error);
-        });
-}
-
-function processSkinOptions(options) {
-    var selectElement = document.querySelector(".select");
-    
-    options.forEach(option => {
-        var optionElement = document.createElement("option");
-        optionElement.value = option.id;
-        optionElement.textContent = option.name;
-        selectElement.appendChild(optionElement);
-    });
-}
-
 function showImage() {
     var selectElement = document.querySelector(".select");
-    var logoImage = document.getElementById("logoImage");
+    var logoImage = document.getElementById("skinImage");
 
-    var selectedOptionId = selectElement.value;
-    // Hier können Sie die ID verwenden, um die entsprechende Skin-URL von der API abzurufen
+    if (selectElement.value === "blau") {
+        logoImage.src = "../../img/skins/blau.png";
+    } else if (selectElement.value === "blau-gelb") {
+        logoImage.src = "../../img/skins/blaugelb.png";
+    } else if (selectElement.value === "Coder") {
+        logoImage.src = "../../img/skins/coder.png";
+    } else if (selectElement.value === "grün") {
+        logoImage.src = "../../img/skins/grün.png";
+    } else if (selectElement.value === "Krabbe") {
+        logoImage.src = "../../img/skins/krabbe.png";
+    } else if (selectElement.value === "orange") {
+        logoImage.src = "../../img/skins/orange.png";
+    } else if (selectElement.value === "Redstone-Kaktus") {
+        logoImage.src = "../../img/skins/redstonekaktus.png";
+    } else if (selectElement.value === "Streifenshirt") {
+        logoImage.src = "../../img/skins/streifen.png";
+    }
     
-    // Beispiel:
-    fetch(`https://api.mineskin.eu/get/id/${selectedOptionId}`)
-        .then(response => response.json())
-        .then(data => {
-            var skinUrl = data.data.texture.url;
-            logoImage.src = skinUrl;
-            logoImage.style.display = "block";
-        })
-        .catch(error => {
-            console.error("Fehler beim Abrufen des Skin-Bildes:", error);
-        });
+    logoImage.style.display = "block";
+}
+
+function showCustomAlert(message) {
+    var customAlert = document.getElementById('custom-alert');
+    var customAlertMessage = document.getElementById('custom-alert-message');
+
+    customAlertMessage.textContent = message;
+    customAlert.style.display = 'block';
+
+    // Verzögertes Ausblenden der benutzerdefinierten Benachrichtigung
+    setTimeout(function() {
+        customAlert.style.display = 'none';
+    }, 3000); // Anzeige für 3 Sekunden
 }
