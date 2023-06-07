32886594b03f6dcd217c9765d558370b7c3ad23a
Julian <79467458+ZehJulianLp@users.noreply.github.com>
2023-06-07 13:27:26 +0200

    Added Checkout

--- shop/cactus/script.js
+++ shop/cactus/script.js
@@ -1,107 +1,99 @@
-// Zugriff auf den Warenkorb aus dem Local Storage
 var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
 function displayCartItems() {
   var cartContainer = document.getElementById('cart-container');
   var totalPrice = 0;
 
-  // Leeren des Inhalts des Warenkorbs
   cartContainer.innerHTML = '';
 
-  // Überprüfen, ob Artikel im Warenkorb vorhanden sind
   if (cartItems.length === 0) {
     cartContainer.innerHTML = '<p>Ihr Kaktus hat noch keine Items zerstört.</p>';
     return;
   }
 
-  // Erstellen der Tabelle für die Anzeige der Artikel im Warenkorb
+  if (cartItems.length > 0) {
+    getCheckout();
+  }
+
+  function getCheckout() {
+    var checkout = document.getElementById('checkout');
+    if (checkout) {
+      checkout.style.display = 'block';
+    }
+  }
+
   var table = document.createElement('table');
   table.classList.add('cart-table');
 
-  // Erstellen der Tabellenüberschriften
   var tableHeader = document.createElement('thead');
   tableHeader.innerHTML = '<tr><th>Name</th><th>Preis</th><th></th><th></th></tr>';
   table.appendChild(tableHeader);
 
-  // Erstellen der Tabellenkörper für die Artikel
   var tableBody = document.createElement('tbody');
 
-  // Schleife über die Artikel im Warenkorb
   for (var i = 0; i < cartItems.length; i++) {
     var item = cartItems[i];
 
-    // Erstellen einer Zeile für den Artikel
     var row = document.createElement('tr');
 
-    // Name des Artikels
     var nameCell = document.createElement('td');
     nameCell.textContent = item.name;
     row.appendChild(nameCell);
 
-    // Preis des Artikels
     var priceCell = document.createElement('td');
     priceCell.textContent = item.price + ' Emeralds';
     row.appendChild(priceCell);
 
-    // Button zum Entfernen des Artikels
     var removeCell = document.createElement('td');
     var removeButton = document.createElement('button');
-    removeButton.textContent = 'Entfernen';
+    removeButton.textContent = 'Dieses entfernen...';
     removeButton.addEventListener('click', removeItem.bind(null, item));
     removeCell.appendChild(removeButton);
     row.appendChild(removeCell);
 
-    // Button zum Hinzufügen des Artikels
     var addCell = document.createElement('td');
     var addButton = document.createElement('button');
-    addButton.textContent = 'Weiteres hinzufügen';
+    addButton.textContent = 'Weiteres hinzufügen...';
     addButton.addEventListener('click', addItem.bind(null, item));
     addCell.appendChild(addButton);
     row.appendChild(addCell);
 
-    // Hinzufügen der Zeile zur Tabellenkörper
     tableBody.appendChild(row);
 
-    // Addieren des Preises zum Gesamtpreis
     totalPrice += item.price;
   }
 
-  // Hinzufügen des Tabellenkörpers zur Tabelle
   table.appendChild(tableBody);
 
-  // Hinzufügen der Tabelle zum Warenkorb-Container
   cartContainer.appendChild(table);
 
-  // Anzeigen des Gesamtpreises
   var totalPriceElement = document.getElementById('total-price');
   totalPriceElement.textContent = 'Gesamtpreis: ' + totalPrice + ' Emeralds';
 }
 
-// Funktion zum Entfernen eines Artikels aus dem Warenkorb
 function removeItem(item) {
-  // Finden des Index des zu entfernenden Artikels
   var index = cartItems.indexOf(item);
   if (index !== -1) {
-    // Entfernen des Artikels aus dem Warenkorb
-
     var removedItem = cartItems.splice(index, 1);
     if (removedItem.length > 0) {
-    var removedItemName = removedItem[0].name;
-    console.log('Artikel "' + removedItemName + '" wurde aus dem Warenkorb entfernt.');
-    // Speichern des aktualisierten Warenkorbs im Local Storage
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-    displayCartItems();
+      var removedItemName = removedItem[0].name;
+      console.log('Artikel "' + removedItemName + '" wurde aus dem Warenkorb entfernt.');
+      localStorage.setItem('cart', JSON.stringify(cartItems));
+      displayCartItems();
+      location.reload(); // Seite aktualisieren
     }
-    }
-    }
-    
-    // Funktion zum Hinzufügen eines Artikels zum Warenkorb
-    function addItem(item) {
-    cartItems.push(item);
-    // Speichern des aktualisierten Warenkorbs im Local Storage
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-    displayCartItems();
-    }
-    
-    // Aufruf der Funktion, um die Artikel im Warenkorb anzuzeigen
-    displayCartItems();
+  }
+}
+
+function addItem(item) {
+  cartItems.push(item);
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+  displayCartItems();
+  location.reload(); // Seite aktualisieren
+}
+
+displayCartItems();
+
+document.getElementById('checkout-button').addEventListener('click', function() {
+  window.open('../checkout/', '_self');
+});
---
+++ shop/checkout/script.js
@@ -0,0 +1,112 @@
+var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+var kaktusContainer = document.getElementById('kaktus-container');
+var progressBar = document.getElementById('progress-bar');
+var progressText = document.getElementById('progress-text');
+
+var totalItems = cartItems.length;
+var currentItemIndex = 0;
+
+function destroyItems() {
+  if (currentItemIndex < totalItems) {
+    var currentItem = cartItems[currentItemIndex];
+    var itemName = currentItem.name;
+
+    var imagePath = generateImagePath(itemName);
+    var itemImage = createItemImage(imagePath);
+
+    var itemFallInterval = setInterval(function() {
+      var topPosition = parseFloat(itemImage.style.top);
+      if (topPosition < -250) {
+        topPosition += 20;
+        itemImage.style.top = topPosition + 'px';
+      } else {
+        clearInterval(itemFallInterval);
+        kaktusContainer.removeChild(itemImage);
+        currentItemIndex++;
+        var progressPercentage = (currentItemIndex / totalItems) * 100;
+        progressBar.style.width = progressPercentage + '%';
+        progressText.textContent = 'Ihre Items werden zerstört... ' + currentItemIndex + ' von ' + totalItems + ' abgeschlossen';
+
+        var currentItem = cartItems[currentItemIndex - 1];
+        var itemName = currentItem.name;
+        playSkinSound(itemName);
+
+        destroyItems();
+        
+        if (currentItemIndex >= totalItems) {
+          setTimeout(function() {
+            showCheckmarkAnimation();
+            clearCartItems();
+          }, 1000); // Warten Sie 1 Sekunde, bevor Sie den Checkmark anzeigen
+        }
+      }
+    }, 100);
+  }
+}
+
+function showCheckmarkAnimation() {
+  var checkmark = document.getElementById('checkmark');
+  checkmark.style.display = 'block';
+}
+
+function clearCartItems() {
+  localStorage.removeItem('cart');
+}
+
+function generateImagePath(itemName) {
+  var imagePath;
+  // Generieren Sie den Bildpfad basierend auf dem Namen des Items
+  if (itemName === 'Dwarslooper-Logo: alt') {
+    imagePath = '../../img/dwarslogoalt.png';
+  } else if (itemName === 'Dwarslooper-Logo: aktuell') {
+    imagePath = '../../img/logo.webp';
+  } else if (itemName === 'Dwarslooper-Skin: blau') {
+    imagePath = '../../img/skins/blau.png';
+  } else if (itemName === 'Dwarslooper-Skin: blau-gelb') {
+    imagePath = '../../img/skins/blaugelb.png';
+  } else if (itemName === 'Dwarslooper-Skin: Coder') {
+    imagePath = '../../img/skins/coder.png';
+  } else if (itemName === 'Dwarslooper-Skin: grün') {
+    imagePath = '../../img/skins/grün.png';
+  } else if (itemName === 'Dwarslooper-Skin: Krabbe') {
+    imagePath = '../../img/skins/krabbe.png';
+  } else if (itemName === 'Dwarslooper-Skin: Redstone-Kaktus') {
+    imagePath = '../../img/skins/redstonekaktus.png';
+  } else if (itemName === 'Dwarslooper-Skin: orange') {
+    imagePath = '../../img/skins/orange.png';
+  } else if (itemName === 'Dwarslooper-Skin: Streifenshirt') {
+    imagePath = '../../img/skins/streifen.png';
+  }  return imagePath;
+}
+function createItemImage(imagePath) {
+    var itemImage = document.createElement('img');
+    itemImage.src = imagePath;
+    itemImage.classList.add('item-image');
+    itemImage.style.position = 'absolute';
+    itemImage.style.left = (kaktusContainer.offsetWidth - itemImage.offsetWidth) / 2 + 'px';
+    itemImage.style.top = '-700px';
+    kaktusContainer.appendChild(itemImage);
+    return itemImage;
+  }
+  
+  function playSkinSound(itemName) {
+    var skinNames = [
+      'Dwarslooper-Skin: blau',
+      'Dwarslooper-Skin: blau-gelb',
+      'Dwarslooper-Skin: Coder',
+      'Dwarslooper-Skin: grün',
+      'Dwarslooper-Skin: Krabbe',
+      'Dwarslooper-Skin: Redstone-Kaktus',
+      'Dwarslooper-Skin: orange',
+      'Dwarslooper-Skin: Streifenshirt'
+    ];
+  
+    if (skinNames.includes(itemName)) {
+      var soundPath = '../../img/oof.mp3';
+      var audio = new Audio(soundPath);
+      audio.play();
+    }
+  }
+  
+  destroyItems();
